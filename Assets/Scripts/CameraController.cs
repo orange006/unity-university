f2b47bea28d3d30f2a0d32be4699453ec0c0ff49
orange006 <alexeyefimov15@gmail.com>
2021-02-07 13:04:44 +0200

    Practice #2

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+
+    public float cameraDistance = 30.0f;
+
+    private void Awake()
+    {
+        GetComponent<Camera>().orthographicSize = ((Screen.height / 2) / cameraDistance);
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float movementSpeed = 5f;
+    public float jumpY = 3f;
+
+    void Update()
+    {
+        Jump();
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        transform.position += movement * Time.deltaTime * movementSpeed;
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpY), ForceMode2D.Impulse);
+        }
+    }
+
+}
