4bffa5d0f9b9cfba10df7ea11f50ff25267a690a
orange006 <alexeyefimov15@gmail.com>
2021-04-07 14:12:26 +0300

    Independent work.

---
+++ Assets/Scenes/GestureRecognizer/Scripts/GestureData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GestureRecognizer
+{
+    [System.Serializable]
+    public class GestureLine
+    {
+        public List<Vector2> points = new List<Vector2>();
+        public bool closedLine;
+    }
+
+    [System.Serializable]
+    public class GestureData
+    {
+        public List<GestureLine> lines = new List<GestureLine>();
+        public GestureLine LastLine { get { return lines[lines.Count - 1]; } }
+    }
+}
---
+++ Assets/Scenes/GestureRecognizer/Scripts/GesturePattern.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GestureRecognizer
+{
+    [CreateAssetMenu(menuName = "GestureRecognizer/GesturePattern")]
+    public class GesturePattern : ScriptableObject
+    {
+        public string id;
+
+        [System.Obsolete("'points' is deprecated, please use 'gesture' instead.")]
+        [HideInInspector]
+        [SerializeField]
+        public List<Vector2> points;
+
+        public GestureData gesture;
+
+        public bool useLinesOrder;
+        public bool useLinesDirections;
+    }
+}
