c1fd1c29c5efaed105e46c804015bc519c15a188
orange006 <alexeyefimov15@gmail.com>
2021-04-11 15:59:55 +0300

    Practice #7

---
+++ Assets/Scripts/GetConfig.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.RemoteConfig;
+using UnityEngine.UI;
+
+public class GetConfig : MonoBehaviour
+{
+    public struct userAttributes { }
+    public struct appAttributes { }
+
+    private string message;
+    private int number;
+
+    [SerializeField] private Text messageText;
+    [SerializeField] private Text numberText;
+
+    private void Awake()
+    {
+        ConfigManager.FetchCompleted += SetValues;
+        ConfigManager.FetchConfigs<userAttributes, appAttributes>
+                 (new userAttributes(), new appAttributes());
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            ConfigManager.FetchConfigs<userAttributes, appAttributes>(
+                          new userAttributes(), new appAttributes());
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+    }
+
+    void SetValues(ConfigResponse response)
+    {
+        message = ConfigManager.appConfig.GetString("TextMessage");
+        number = ConfigManager.appConfig.GetInt("RemoteNumber");
+
+        messageText.text = message;
+        numberText.text = "Remote Number is: " + number.ToString();
+    }
+
+    private void OnDestroy()
+    {
+        ConfigManager.FetchCompleted -= SetValues;
+    }
+}
